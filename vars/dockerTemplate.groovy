f584717426f4be94761ce138ac77ff85ffaf5f11
Arturo Contreras <arturo.contreras@returnpath.com>
2017-05-19 13:10:55 -0600

    Fix

--- vars/dockerTemplate.groovy
+++ vars/dockerTemplate.groovy
@@ -1,10 +1,7 @@
 #!/usr/bin/groovy
 def call(Map parameters = [:], body) {
-    def defaultLabel = buildId('docker')
-    def label = parameters.get('label', defaultLabel)
-
     def dockerImage = parameters.get('dockerImage', 'docker:1.11')
-      podTemplate(label: label,
+      podTemplate(label: 'docker',
             containers: [[name: 'docker', image: "${dockerImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true]],
             volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
             envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock']]) {
