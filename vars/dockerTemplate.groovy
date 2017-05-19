092c8a5c8cdb88ee91f4254cc05926e77484d36e
Arturo Contreras <arturo.contreras@returnpath.com>
2017-05-19 13:37:12 -0600

    Fix

--- vars/dockerTemplate.groovy
+++ vars/dockerTemplate.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/groovy
 def call(body) {
-podTemplate(label: label,
+podTemplate(label: 'docker',
         containers: [containerTemplate(name: 'docker', image: 'docker', command: 'cat', ttyEnabled: true)],
         volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
     body()
