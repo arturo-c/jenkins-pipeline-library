9f1666bf73161189491612d66dc6535cc4993b67
Arturo Contreras <arturo.contreras@returnpath.com>
2017-05-19 13:27:07 -0600

    Maybe

--- vars/dockerTemplate.groovy
+++ vars/dockerTemplate.groovy
@@ -2,9 +2,9 @@
 def call(Map parameters = [:], body) {
     def dockerImage = parameters.get('dockerImage', 'docker:1.11')
       podTemplate(label: 'docker',
-            containers: [[name: 'docker', image: "${dockerImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true]],
-            volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
-            envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock']]) {
+            containers: [[name: 'docker', image: "${dockerImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true, workingDir: '/home/jenkins/', envVars: [[key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']]]],
+            volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock'), secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker')],
+            envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock'], [key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']]) {
 
             body()
 
