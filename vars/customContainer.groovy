0e8769a5bd8263728f627bdd1cc275bc82e36a48
Arturo Contreras <arturo.contreras@returnpath.com>
2017-05-23 11:49:30 -0600

    Add ksh.

--- vars/customContainer.groovy
+++ vars/customContainer.groovy
@@ -4,21 +4,3 @@ def customContainer(String name, Closure body) {
                 body()
         }
 }
-
-def ksh(command) {
-        if (env.CONTAINER_NAME) {
-                if ((command instanceof String) || (command instanceof GString)) {
-                        command = kubectl(command)
-                }
-
-                if (command instanceof LinkedHashMap) {
-                        command["script"] = kubectl(command["script"])
-                }
-        }
-
-        sh(command)
-}
-
-def kubectl(command) {
-        "kubectl exec -i ${env.HOSTNAME} -c ${env.CONTAINER_NAME} -- /bin/sh -c 'cd ${env.WORKSPACE} && DOCKER_API_VERSION=1.23 ${command}'"
-}
