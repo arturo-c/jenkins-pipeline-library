b4e1fcc8d405ef5c56828012fffa749465932c9e
Arturo Contreras <arturo.contreras@returnpath.com>
2017-05-23 11:43:29 -0600

    Adding custom container command.

--- vars/deployTemplate.groovy
+++ vars/deployTemplate.groovy
@@ -10,27 +10,3 @@ def call(body) {
         }
     }
 }
-
-def ksh(command) {
-	if (env.CONTAINER_NAME) {
-		if ((command instanceof String) || (command instanceof GString)) {
-			command = kubectl(command)
-		}
-
-		if (command instanceof LinkedHashMap) {
-			command["script"] = kubectl(command["script"])
-		}
-	}
-
-	sh(command)
-}
-
-def kubectl(command) {
-	"kubectl exec -i ${env.HOSTNAME} -c ${env.CONTAINER_NAME} -- /bin/sh -c 'cd ${env.WORKSPACE} && DOCKER_API_VERSION=1.23 ${command}'"
-}
-
-def customContainer(String name, Closure body) {
-	withEnv(["CONTAINER_NAME=$name"]) {
-		body()
-	}
-}
